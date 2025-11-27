e0a7d47b0a824d629f016113749ee79878ad22a2
Guilherme Makiyama <makiyamagui@gmail.com>
2025-11-27 00:01:16 -0300

    fix: logout

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -34,7 +34,7 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
     sessionStorage.removeItem('accessToken');
 
     if (pathname !== '/login') {
-      redirect('/login');
+      router.replace('/login');
     }
   };
 
